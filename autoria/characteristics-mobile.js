b8f7986c5b1e6c9275fe32301614fed2bd415c4a
dragonegor <dragonegor@ukr.net>
2020-11-01 22:26:34 +0200

    autoria change

--- autoria/characteristics-mobile.js
+++ autoria/characteristics-mobile.js
@@ -96,8 +96,10 @@ document.body.insertAdjacentHTML('afterbegin', style);
 document.querySelector('.defines_list>*:nth-child(8)').insertAdjacentHTML('afterend', fuelInfo);
 document.querySelector('.characteristics').insertAdjacentHTML('afterend', moreCharacteristics);
 let complectationBlock = document.querySelector('.auto-complectation-block');
-complectationBlock.remove();
+let char = document.querySelector('.more-characteristics');
+char.after(complectationBlock);
 console.log(complectationBlock);
+console.log(char);
 
 
 
