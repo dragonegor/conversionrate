6caea0cb312a9021bcd25a21a69a5bf18f7516fe
dragonegor <39415597+dragonegor@users.noreply.github.com>
2020-11-02 13:16:56 +0200

    Update characteristics-mobile.js

--- autoria/characteristics-mobile.js
+++ autoria/characteristics-mobile.js
@@ -136,28 +136,38 @@ char.after(complectationBlock);
 
 document.querySelector('#auto-complectation-block h3').innerHTML = `Інші варіанти комплектації ${carModel}:`;
 document.querySelector('#auto-complectation-block ul').insertAdjacentHTML('afterend', equipment);
-
+let mut = new MutationObserver(function () {
+  if(this.classList.contains('fuel_info')) {
+    document.querySelector('.react_modal__header').innerHTML = `
+      <h4>Дізнатися про фактичні витрати палива ${carModel}</h4>
+      <p>Офіційний сервіс автосалону може надати вам <br><b>реальні дані щодо витрат палива,</b><br>грунтуючись на авто, яких вони обслуговують.</p>
+      <span class="react_modal__close">×</span>`
+  } else if (this.classList.contains('char')) {
+    document.querySelector('.react_modal__header').innerHTML = `
+      <h4>Уточнити характеристики ${carModel}</h4>
+      <p>Запросіть інформацію про авто, якої бракує на сайті.</p>
+      <span class="react_modal__close">×</span>`
+  } else if (this.classList.contains('equip')) {
+    document.querySelector('.react_modal__header').innerHTML = `
+      <h4>Дізнатися про поставки ${carModel}</h4>
+      <p>Запросіть інформацію про найближчі поставки даного авто (у наступні 30 днів).</p>
+      <span class="react_modal__close">×</span>`
+  }
+})
 document.querySelectorAll('.test-popup').forEach((item)=> {
   item.addEventListener('click', function () {
+
+    mut.observe(document.documentElement, {
+      attributes: true,
+      characterData: true,
+      childList: true,
+      subtree: true,
+      attributeOldValue: true,
+      characterDataOldValue: true
+    });
+
     let event = new Event('click', {bubbles: true});
     document.querySelector("span.button.button--green").dispatchEvent(event);
-    setTimeout(() => {
-      if(this.classList.contains('fuel_info')) {
-        document.querySelector('.react_modal__header').innerHTML = `
-        <h4>Дізнатися про фактичні витрати палива ${carModel}</h4>
-        <p>Офіційний сервіс автосалону може надати вам <br><b>реальні дані щодо витрат палива,</b><br>грунтуючись на авто, яких вони обслуговують.</p>
-        <span class="react_modal__close">×</span>`
-      } else if (this.classList.contains('char')) {
-        document.querySelector('.react_modal__header').innerHTML = `
-        <h4>Уточнити характеристики ${carModel}</h4>
-        <p>Запросіть інформацію про авто, якої бракує на сайті.</p>
-        <span class="react_modal__close">×</span>`
-      } else if (this.classList.contains('equip')) {
-        document.querySelector('.react_modal__header').innerHTML = `
-        <h4>Дізнатися про поставки ${carModel}</h4>
-        <p>Запросіть інформацію про найближчі поставки даного авто (у наступні 30 днів).</p>
-        <span class="react_modal__close">×</span>`
-      }
-    }, 100)
+
   })
 })
