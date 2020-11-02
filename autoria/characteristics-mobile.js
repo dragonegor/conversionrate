ac078b521d1d18ab4089e5f07fd1c16352a627e5
dragonegor <39415597+dragonegor@users.noreply.github.com>
2020-11-02 12:32:33 +0200

    Update characteristics-mobile.js

--- autoria/characteristics-mobile.js
+++ autoria/characteristics-mobile.js
@@ -143,17 +143,17 @@ document.querySelectorAll('.test-popup').forEach((item)=> {
     document.querySelector("span.button.button--green").dispatchEvent(event);
 
     if(this.classList.contains('fuel_info')) {
-      document.querySelector('react_modal__header').innerHTML = `
+      document.querySelector('.react_modal__header').innerHTML = `
 <h4>Дізнатися про фактичні витрати палива ${carModel}</h4>
 <p>Офіційний сервіс автосалону може надати вам <br><b>реальні дані щодо витрат палива,</b><br>грунтуючись на авто, яких вони обслуговують.</p>
 <span class="react_modal__close">×</span>`
     } else if (this.classList.contains('char')) {
-      document.querySelector('react_modal__header').innerHTML = `
+      document.querySelector('.react_modal__header').innerHTML = `
 <h4>Уточнити характеристики ${carModel}</h4>
 <p>Запросіть інформацію про авто, якої бракує на сайті.</p>
 <span class="react_modal__close">×</span>`
     } else if (this.classList.contains('equip')) {
-      document.querySelector('react_modal__header').innerHTML = `
+      document.querySelector('.react_modal__header').innerHTML = `
 <h4>Дізнатися про поставки ${carModel}</h4>
 <p>Запросіть інформацію про найближчі поставки даного авто (у наступні 30 днів).</p>
 <span class="react_modal__close">×</span>`
