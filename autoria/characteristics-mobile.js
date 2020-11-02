b39b3a18edc3ba1ff7831960a0c8ef2bd5a9959e
dragonegor <39415597+dragonegor@users.noreply.github.com>
2020-11-02 14:24:45 +0200

    Update characteristics-mobile.js

--- autoria/characteristics-mobile.js
+++ autoria/characteristics-mobile.js
@@ -137,21 +137,33 @@ char.after(complectationBlock);
 document.querySelector('#auto-complectation-block h3').innerHTML = `Інші варіанти комплектації ${carModel}:`;
 document.querySelector('#auto-complectation-block ul').insertAdjacentHTML('afterend', equipment);
 
-let mut = new MutationObserver(function (mutations) {
-  console.log(mutations);
-  console.log(this);
-    document.querySelector('.react_modal__header').innerHTML = `
-        <h4>Дізнатися про фактичні витрати палива ${carModel}</h4>
-        <p>Офіційний сервіс автосалону може надати вам <br><b>реальні дані щодо витрат палива,</b><br>грунтуючись на авто, яких вони обслуговують.</p>
-        <span class="react_modal__close">×</span>`
-  
-  mut.disconnect();
-})
-
 document.querySelectorAll('.test-popup').forEach((item)=> {
   item.addEventListener('click', function () {
 
-    mut.observe(document.body, {
+    let mut = new MutationObserver((mutations) => {
+      console.log(mutations[0].target);
+      console.log(this);
+      if(this.classList.contains('fuel_info')) {
+        document.querySelector('.react_modal__header').innerHTML = `
+        <h4>Дізнатися про фактичні витрати палива ${carModel}</h4>
+        <p>Офіційний сервіс автосалону може надати вам <br><b>реальні дані щодо витрат палива,</b><br>грунтуючись на авто, яких вони обслуговують.</p>
+        <span class="react_modal__close">×</span>`
+      } else if (this.classList.contains('char')) {
+        document.querySelector('.react_modal__header').innerHTML = `
+        <h4>Уточнити характеристики ${carModel}</h4>
+        <p>Запросіть інформацію про авто, якої бракує на сайті.</p>
+        <span class="react_modal__close">×</span>`
+      } else if (this.classList.contains('equip')) {
+        document.querySelector('.react_modal__header').innerHTML = `
+        <h4>Дізнатися про поставки ${carModel}</h4>
+        <p>Запросіть інформацію про найближчі поставки даного авто (у наступні 30 днів).</p>
+        <span class="react_modal__close">×</span>`
+      }
+
+      mut.disconnect();
+    })
+
+    mut.observe.bind(this, document.body, {
       characterData: true,
       childList: true,
       subtree: true,
@@ -160,5 +172,6 @@ document.querySelectorAll('.test-popup').forEach((item)=> {
 
     let event = new Event('click', {bubbles: true});
     document.querySelector("span.button.button--green").dispatchEvent(event);
+    
   })
 })
