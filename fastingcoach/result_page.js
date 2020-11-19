a6e1dad3f0284372b5b5d8b66564e486fe289342
dragonegor <39415597+dragonegor@users.noreply.github.com>
2020-11-19 14:10:17 +0200

    Update result_page.js

--- fastingcoach/result_page.js
+++ fastingcoach/result_page.js
@@ -1,7 +1,3 @@
-let scriptSlider = document.createElement('script');
-scriptSlider.src = 'https://cdnjs.cloudflare.com/ajax/libs/tiny-slider/2.9.3/min/tiny-slider.js';
-scriptSlider.async = true;
-document.head.appendChild(scriptSlider);
 
 let style = `
     <style>
