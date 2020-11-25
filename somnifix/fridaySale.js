6bd3b6eeb4b6bd513a72daf124e0ed0836d7674e
dragonegor <39415597+dragonegor@users.noreply.github.com>
2020-11-25 13:38:20 +0200

    Update fridaySale.js

--- somnifix/fridaySale.js
+++ somnifix/fridaySale.js
@@ -52,7 +52,7 @@ let lite = `
 <span class="old-price-label">$19.97</span>
 `
 document.body.insertAdjacentHTML('afterbegin', style)
-// window.onload =  function () {
+window.onload =  function () {
 
     document.querySelectorAll('.on-variant-label[data-index="1"] .on-price')[0].style.opacity = '0'
     document.querySelectorAll('.on-variant-label[data-index="2"] .on-price')[0].style.opacity = '0'
@@ -153,4 +153,4 @@ document.body.insertAdjacentHTML('afterbegin', style)
             subtree: true
         })
     })
-// }
+}
