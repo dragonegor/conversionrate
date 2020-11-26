b8eaa971b10625f42b7fee13f7a77d5e7d1b0334
dragonegor <39415597+dragonegor@users.noreply.github.com>
2020-11-26 18:20:45 +0200

    Update top25.js

--- moonmagic/top25.js
+++ moonmagic/top25.js
@@ -1,3 +1,5 @@
 if(window.location.href !== 'https://shop.moonmagic.com/collections/jewelry?pg=12&sort_by=none&select_store=1') {
   window.location.href = 'https://shop.moonmagic.com/collections/jewelry?pg=12&sort_by=none&select_store=1'
 }
+
+console.log('work')
