32f2f4523f7e08b6bb44b3e5e417643d001fe90f
dragonegor <39415597+dragonegor@users.noreply.github.com>
2020-11-26 20:20:54 +0200

    Update top25.js

--- moonmagic/top25.js
+++ moonmagic/top25.js
@@ -1,5 +1,16 @@
+let style = `
+<style>
+  #gridprod-4508411330632 {
+    order: -1;
+}
+</style>
+`
+
+
 if(window.location.href !== 'https://moonmagic.com/collections/jewelry?pg=12&sort_by=none&select_store=1') {
-  window.location.href = 'https://moonmagic.com/collections/jewelry?pg=12&sort_by=none&select_store=1'
+    window.location.href = 'https://moonmagic.com/collections/jewelry?pg=12&sort_by=none&select_store=1'
+} else {
+    document.body.insertAdjacentHTML('afterbegin', style)
+    console.log('sorting products')
 }
 
-console.log('work')
