d2c10fe1477fa32300b70a1b85d0c46c25395881
dragonegor <39415597+dragonegor@users.noreply.github.com>
2020-11-26 18:20:22 +0200

    Create top25.js

---
+++ moonmagic/top25.js
@@ -0,0 +1,3 @@
+if(window.location.href !== 'https://shop.moonmagic.com/collections/jewelry?pg=12&sort_by=none&select_store=1') {
+  window.location.href = 'https://shop.moonmagic.com/collections/jewelry?pg=12&sort_by=none&select_store=1'
+}
