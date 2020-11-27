6de8c7568257965ed8480f4a170334b1c4bd448a
dragonegor <39415597+dragonegor@users.noreply.github.com>
2020-11-27 09:33:26 +0200

    Update top25.js

--- moonmagic/top25.js
+++ moonmagic/top25.js
@@ -1,8 +1,19 @@
 let style = `
 <style>
-  #gridprod-4508411330632,
+  #gridprod-3982651555912,
+  #gridprod-3481223692360,
+  #gridprod-3982601945160,
+  #gridprod-3481349423176,
+  #gridprod-4508390686792,
+  #gridprod-3982926676040,
+  #gridprod-3481362956360,
+  #gridprod-4091207843912,
+  #gridprod-3481221529672,
+  #gridprod-3984110354504,
+  #gridprod-4090238304328,
+  #gridprod-4091200274504,
   #gridprod-3481223495752,
-  #gridprod-4091200274504 {
+  #gridprod-4508411330632 {
     order: -1;
   }
 </style>
