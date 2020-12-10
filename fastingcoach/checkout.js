200140a089b8f5db8db10500caa2fd1eefadde8a
dragonegor <39415597+dragonegor@users.noreply.github.com>
2020-12-10 13:48:26 +0200

    Update checkout.js

--- fastingcoach/checkout.js
+++ fastingcoach/checkout.js
@@ -187,5 +187,25 @@ let block = `
 document.addEventListener("DOMContentLoaded", function () {
     document.body.insertAdjacentHTML('afterbegin', style);
     document.querySelector('.checkout-reserved').insertAdjacentHTML('afterend', block);
+    window.dataLayer = window.dataLayer || [];
+    dataLayer.push({
+        'event': 'event-to-ga',
+        'eventCategory': 'Exp: Checkout page copy',
+        'eventAction': 'loaded'
+    });
 })
+
+try {
+    (function(h,o,t,j,a,r){
+        h.hj=h.hj||function(){(h.hj.q=h.hj.q||[]).push(arguments)};
+        h._hjSettings={hjid:1962080,hjsv:6};
+        a=o.getElementsByTagName('head')[0];
+        r=o.createElement('script');r.async=1;
+        r.src=t+h._hjSettings.hjid+j+h._hjSettings.hjsv;
+        a.appendChild(r);
+    })(window,document,'https://static.hotjar.com/c/hotjar-','.js?sv=');
+    window.hj = window.hj || function(){(hj.q = hj.q || []).push(arguments)};
+    hj('trigger', 'checkout_page_copy');
+}
+catch(e) {}
 
