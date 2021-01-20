dbffc53aac9e98a77e2934a3688d3903c9481b73
dragonegor <39415597+dragonegor@users.noreply.github.com>
2021-01-20 12:17:57 +0200

    Update cer_checkout.js

--- cerebelly/cer_checkout.js
+++ cerebelly/cer_checkout.js
@@ -181,11 +181,13 @@ function redrawing() {
         let totalSum = document.querySelector('.sum-row.order-total .total').innerHTML
 
         if (!document.querySelector('.promo_btn')) {
-            console.log('!!!!')
+            
             document.querySelector('.sum-row.total:not(.coupon)').insertAdjacentHTML('afterend', block)
             if (!document.querySelector('.sum-row.total.coupon:nth-child(8)')) {
                 document.querySelector('.first_discount').style.display = 'none'
+                console.log('!!!!')
             } else {
+                console.log('!!!!')
                 document.querySelector('.first_discount .total_sum').innerHTML = totalSum
                 document.querySelector('.first_discount .close_first_discount').addEventListener('click', function () {
                     document.querySelector('.first_discount').style.display = 'none'
