1ca261cb65ab0e43c930335e8d3dd09412794ebc
dragonegor <39415597+dragonegor@users.noreply.github.com>
2021-01-29 11:36:23 +0200

    Update free_trial.js

--- somnifix/free_trial.js
+++ somnifix/free_trial.js
@@ -241,7 +241,7 @@ document.addEventListener('DOMContentLoaded', function () {
         a.appendChild(r);
     })(window,document,'https://static.hotjar.com/c/hotjar-','.js?sv=');
     window.hj=window.hj||function(){(hj.q=hj.q||[]).push(arguments)};
-    hj('trigger', 'free_trial');
+    hj('trigger', 'optimize_pdp');
 
     window.dataLayer = window.dataLayer || [];
     dataLayer.push({
