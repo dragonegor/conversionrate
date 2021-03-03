adf375516c4f97178ac4895d45e1abaf1c90e7bc
dragonegor <39415597+dragonegor@users.noreply.github.com>
2021-03-03 14:54:56 +0200

    Update pdp_improvements.js

--- mdnt/pdp_improvements.js
+++ mdnt/pdp_improvements.js
@@ -278,7 +278,7 @@ window.onload = function () {
 
 
 
-    $('.blaster-select--Size').append(`<div class="show_size_chart">Size Guige</div>`)
+    $('.blaster-select--Size').append(`<div class="show_size_chart">Size Guide</div>`)
     setTimeout(function () {
         document.querySelectorAll('.ProductCardMultiselect_picky-product-options-container_1fxk3>div').forEach((item) => {
             if(!item.innerHTML.includes('Size')){
